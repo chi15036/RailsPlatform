c8c7b467236dd4214faae8a4af5c1a8d8171f000
chi <chi15036@gmail.com>
2017-01-04 17:20:45 +0800

    Add cut word

--- app/controllers/v1/entities_controller.rb
+++ app/controllers/v1/entities_controller.rb
@@ -1,5 +1,6 @@
 class V1::EntitiesController < ApplicationController
-  require 'net/http'
+
+  @@Authorization = "Bearer 3EJEHFVZ7YUPQF3HFMBOGAK2UYWD5PFG"
 
   def index
     respond_to do |format|
@@ -8,13 +9,14 @@ class V1::EntitiesController < ApplicationController
   end
 
   def show
-    sentence = URI.encode(params[:sentence])
+    words, flags = cutWord(params[:sentence])
+    sentence = URI.encode(words.join(" "))
     uri = URI.parse("https://api.wit.ai/message?v=20160526&q=" + sentence)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     request = Net::HTTP::Get.new(uri.request_uri)
     #set header
-    request["Authorization"] = "Bearer VWZA77MGCI5NFC633GUSJ2TKZGKB5DRP"
+    request["Authorization"] = @@Authorization
     request["Content-Type"] = "application/json"
 
     response = http.request(request)
@@ -29,9 +31,11 @@ class V1::EntitiesController < ApplicationController
     entities.each do |entity|
       uri = URI.parse("https://api.wit.ai/entities/" + entity[:id] + "/values?v=20160526")
       response = postData(uri, entity[:values][0].to_json)
-      if response[:error]
+      puts response.body.inspect
+      if JSON.parse(response.body).key?("error")
         uri = URI.parse("https://api.wit.ai/entities?v=20160526")
-        response = postData(uri, entity)
+        response = postData(uri, entity.to_json)
+        puts response.body.inspect
       end
     end
     respond_to do |format|
@@ -45,12 +49,34 @@ class V1::EntitiesController < ApplicationController
     http.use_ssl = true
     request = Net::HTTP::Post.new(uri.request_uri)
     #set header
-    request["Authorization"] = "Bearer VWZA77MGCI5NFC633GUSJ2TKZGKB5DRP"
+    request["Authorization"] = @@Authorization
     request["Content-Type"] = "application/json"
     # puts entity[:values][0].inspect
     request.body = body
     response = http.request(request)
     return response
   end
+
+  private
+  def cutWord(sentence)
+    RubyPython.start # start the Python interpreter
+    sys = RubyPython.import("sys")
+    sys.path.append("#{Rails.root}/lib")
+    taiba = RubyPython.import('Taiba')
+    cut_words = taiba.lcut(sentence, true).to_a  #我也不知為何第二個參數一定要true...
+    puts cut_words
+    words = []
+    flags = []
+    cut_words.each do |word|
+      word, flag = word.to_s.split('/')
+      words << word
+      flags << flag
+    end
+    puts words
+    return words, flags
+    # # RubyPython.stop # stop the Python interpreter
+    # result = exec("python #{Rails.root}/lib/Taiba/main.py #{sentence}")
+  end
+
 
 end
